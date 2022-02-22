6b9691ff09f9c8154cf1d4c0abe92e9384790a04
Enrique Piñero <epinerof@us.es>
2022-02-22 17:00:56 +0100

    addresses map change to static to ensure global scope Struct controller removed -- not necessary controller is now a map <string,int> Added address member to EDScorbotJoint Added id member to EDScorbotJoint Changed constructors to populate map instead of struct Removed commented getters/setters Changed declarations of functions to better ones (sendRef,readJoints)

--- c/src/include/EDScorbot.hpp
+++ c/src/include/EDScorbot.hpp
@@ -5,27 +5,17 @@
 using namespace std;
 using json = nlohmann::json;
 
-map<string, int> addresses = {
-    {"M1", 0x00},
-    {"M2", 0x20},
-    {"M3", 0x40},
-    {"M4", 0x60},
-    {"M5", 0x80},
-    {"M6", 0xA0}};
+static map<string, int> addresses = {
+        {"M1", 0x00},
+        {"M2", 0x20},
+        {"M3", 0x40},
+        {"M4", 0x60},
+        {"M5", 0x80},
+        {"M6", 0xA0}};
+
 
 // Data structure to hold SPID configuration for 1 joint
-typedef struct controller
-{
-    int EI_FD_bank3_18bits;
-    int PD_FD_bank3_22bits;
-    int PI_FD_bank3_18bits;
-    int leds;
-    int ref;
-    int spike_expansor;
-    string address;
-    string id;
 
-} SPIDController;
 
 // Class to implement joint functionalities
 class EDScorbotJoint
@@ -35,55 +25,49 @@ private:
     // Declaring
 
 public:
-    SPIDController controller;//Dejar privado? En ese caso habria que hacer getters y setters :(
+    int address;
+    string id;
+    map<string, int> controller = {
+        {"EI_FD_bank3_18bits", 0},
+        {"PD_FD_bank3_22bits", 0},
+        {"PI_FD_bank3_18bits", 0},
+        {"leds", 0},
+        {"ref", 0},
+        {"spike_expansor", 0}};
 
-    EDScorbotJoint(){};
-    EDScorbotJoint(int EI_FD, int PD_FD, int PI_FD, int leds, int spike_exp, string address, string id)
+
+    EDScorbotJoint(int EI_FD, int PD_FD, int PI_FD, int leds, int spike_exp, int address, string id)
     {
-        controller.EI_FD_bank3_18bits = EI_FD;
-        controller.PD_FD_bank3_22bits = PD_FD;
-        controller.PI_FD_bank3_18bits = PI_FD;
-        controller.leds = leds;
-        controller.ref = 0;
-        controller.spike_expansor = spike_exp;
-        controller.address = address;
-        controller.id = id;
+        controller["EI_FD_bank3_18bits"] = EI_FD;
+        controller["PD_FD_bank3_22bits"] = PD_FD;
+        controller["PI_FD_bank3_18bits"] = PI_FD;
+        controller["leds"] = leds;
+        controller["ref"] = 0;
+        controller["spike_expansor"] = spike_exp;
+        this->id = id;
+        this->address = addresses[id];
     };
     EDScorbotJoint(string id)
     {
-        controller.id = id;
-        controller.address = addresses[id];
+        this->id = id;
+        this->address = addresses[id];
     }
     ~EDScorbotJoint(){};
     void configureInit();
     void configureSPID();
-    // void setControllerConfig(int, int, int, int, int, string, string);
-    // void setEI_FD(int);
-    // void setPD_FD(int);
-    // void setPI_FD(int);
-    // void setLeds(int);
-    // void setRef(int);
-    // void setSpikeExpansor(int);
-    // void getMotorAddress();
-    // void getId(int);
-    // void getControllerConfig(int, int, int, int, int, string, string);
-    // void getEI_FD(int);
-    // void getPD_FD(int);
-    // void getPI_FD(int);
-    // void getLeds(int);
-    // void getRef(int);
-    // void getSpikeExpansor(int);
-    // void getMotorAddress(int);
-    // void getId(int);
+
 };
 
 class EDScorbot
 {
 
 private:
-    EDScorbotJoint j1 = {"M1"}, j2 = {"M2"}, j3 = {"M3"},j4 = {"M4"},j5 = {"M5"},j6 = {"M6"};
+
+    int* bram_ptr;
 
 public:
+    EDScorbotJoint j1 = {"M1"}, j2 = {"M2"}, j3 = {"M3"}, j4 = {"M4"}, j5 = {"M5"}, j6 = {"M6"};
+
     EDScorbot();
     EDScorbot(string);
     ~EDScorbot(){};
@@ -91,8 +75,8 @@ public:
     void configureSPID(EDScorbotJoint);
     void configureInit(EDScorbotJoint);
     void searchHome(EDScorbotJoint);
-    void sendRef(int, EDScorbotJoint);
-    int readJoint(EDScorbotJoint);
+    int sendRef(int, EDScorbotJoint);
+    array<int,6> readJoints();
     void resetCounter(EDScorbotJoint);
     void configureLeds(int, EDScorbotJoint);
     void sendFPGAReset();
