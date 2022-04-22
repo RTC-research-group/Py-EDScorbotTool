7a08f80f03324280ad2541fc93f5091c2abab726
Enrique Piñero <epinerof@us.es>
2022-04-22 15:40:07 +0200

    Added features TO BE TESTED

--- l2l/execute_trajectory.py
+++ l2l/execute_trajectory.py
@@ -1,3 +1,4 @@
+from nis import match
 import sys
   
 # setting path
@@ -7,29 +8,68 @@ from pyAER import pyEDScorbotTool
 import argparse
 from transformations import w_to_angles
 import numpy as np
+from scorbot_direct_kinematics import direct_kinematics_position
 
 if __name__== '__main__':
 
     parser = argparse.ArgumentParser(description="Trajectory execution tool to include ED-Scorbot in the L2L loop",allow_abbrev=True)
     parser.add_argument("trajectory",type=str,help="Trajectory file in NumPy format. By default, trajectories should be specified in angular velocities")
-    parser.add_argument("--noconv","-n",help="Specify this flag to indicate that the input trajectory should not be converted to angles",action="store_const",const=False)
-    
+    parser.add_argument("--noconv","-n",help="Specify this flag to indicate that the input trajectory should not be converted to angles",action="store_const",const=True,default=False)
+    parser.add_argument("--counters","-c",type=str,help="Name of the file in which we will store the time-stepped output in counter format",default=None)
+    parser.add_argument("--position","-xyz",type=str,help="Name of the file in which we will store the time-stepped output in xyz format",default=None)
+
     args = parser.parse_args()
 
     traj = np.load(args.trajectory,allow_pickle=True)
 
-    if args.noconv == None:#Esto significa que hay que hacer la conversión
+    if not args.noconv:#Esto significa que hay que hacer la conversión
         traj = w_to_angles(traj)
         pass
 
     #Asumimos que el robot ha hecho el home previamente y que todo esta inicializado
     #Ejecucion de la trayectoria
+    match_steps = []
     handler = pyEDScorbotTool(visible=False)
+    sleep = 0.25
+    handler.toggle_record()
     for j1,j2 in traj:
+        match_steps.append([handler.Read_J1_pos(),handler.Read_J2_pos(),handler.Read_J3_pos(),handler.Read_J4_pos()])
         handler.SendCommandJoint1_lite(j1)
         handler.SendCommandJoint2_lite(j2)
-        t = handler.millis_now()
-        while()
+       
+        t1 = handler.millis_now()
+        t2 = t1
+        while not ((t2-t1)>=sleep*1000):
+            handler.update()
+            t2 = handler.millis_now()
+        
         pass
-
+    handler.update()
+    handler.toggle_record()
     pass
+
+    xyz_steps = []
+    for j1,j2,j3,j4 in match_steps:
+        a1 = handler.count_to_angle(j1)
+        a2 = handler.count_to_angle(j2)
+        xyz = direct_kinematics_position(j1,j2,0,0)
+        xyz_steps.append(xyz)
+
+    print(xyz_steps)
+    print(match_steps)
+
+    match_steps = np.array(match_steps)
+    xyz_steps = np.array(xyz_steps)
+
+    if args.counters == None:
+        np.save("./counters_out.npy",match_steps,allow_pickle=True)
+    else:
+        np.save(args.counters,match_steps,allow_pickle=True)
+    
+    if args.position == None:
+        np.save("./xyz_out.npy",xyz_steps,allow_pickle=True)
+    else:
+        np.save(args.position,xyz_steps,allow_pickle=True)
+
+    
+    
