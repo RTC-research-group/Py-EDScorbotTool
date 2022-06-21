dbd4ffc1709611d0776a083a41615173b6c39c94
Enrique Piñero <epinerof@us.es>
2022-06-21 02:30:23 +0200

    MQTT clients

---
+++ mqtt/client_traj.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+#Send trajectory and wait for responses, until a 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("commands")
+    
+    client.publish("traj_exec","[j1,j2,j3,j4,j5,j6],500",qos=1)
+    
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.0.149", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
