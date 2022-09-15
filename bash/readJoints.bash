896def21ba163bab3bcf348988c308206b150b50
Enrique Piñero <epinerof@us.es>
2022-09-15 13:29:19 +0200

    Updated readJoints.bash

--- bash/readJoints.bash
+++ bash/readJoints.bash
@@ -4,24 +4,24 @@
 #Usage: ReadJoint.bash <joint>
 
 
-declare -A addressPerJoint
-addressPerJoint[1]=0xF1
-addressPerJoint[2]=0xF2
-addressPerJoint[3]=0xF3
-addressPerJoint[4]=0xF4
-addressPerJoint[5]=0xF5
-addressPerJoint[6]=0xF6
+# declare -A addressPerJoint
+# addressPerJoint[1]=0xF1
+# addressPerJoint[2]=0xF2
+# addressPerJoint[3]=0xF3
+# addressPerJoint[4]=0xF4
+# addressPerJoint[5]=0xF5
+# addressPerJoint[6]=0xF6
 
-j=$1
+# j=$1
 
-address=${addressPerJoint[$j]}
+#address=${addressPerJoint[$j]}
 
-data1="0x00F10000"
-data2="0x00F20000"
-data3="0x00F30000"
-data4="0x00F40000"
-data5="0x00F50000"
-data6="0x00F60000"
+data1="0x40000004"
+data2="0x40000008"
+data3="0x4000000C"
+data4="0x40000010"
+data5="0x40000014"
+data6="0x40000018"
 
 ret1=`devmem $data1`
 ret2=`devmem $data2`
