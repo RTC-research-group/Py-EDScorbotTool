0a804742499fc8c7e82a527d845aeba0f8ff5848
Enrique Piñero <epinerof@us.es>
2022-10-20 17:53:07 +0200

    Added argument parser Added verbose parameter to searchHome calls

--- c/src/scripts/home.cpp
+++ c/src/scripts/home.cpp
@@ -1,39 +1,42 @@
 #include "include/EDScorbot.hpp"
 #include <time.h>
 #include <unistd.h>
+#include <argparse/argparse.hpp>
 
-int main(int argc, char *argv[])
-{
-    // int j = atoi(argv[1]);
-    // int ref = atoi(argv[2]);
-    // int init = atoi(argv[3]);
-    char *config_file = argv[1];
+
+int main(int argc, char* argv[])
+{   
+    
+    argparse::ArgumentParser parser("reset");
+    parser.add_argument("-c", "--config_file").help("Optional. Configuration file in JSON format. This file can be used to configure each joint's controller parameters. Default is 'initial_config.json'").default_value(std::string("initial_config.json"));
+    parser.add_argument("-v", "--verbose").help("Increase verbosity of output").default_value(false).implicit_value(true);
+
+    try
+    {
+        parser.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
+
+    const char *config_file = parser.get<std::string>("--config_file").c_str();
+    bool verbose = parser.get<bool>("--verbose");
     EDScorbot handler(config_file);
 
     EDScorbotJoint *joint;
-    // switch (j)
-    // {
-    // case 1:joint = &handler.j1;break;
-    // case 2:joint = &handler.j2;break;
-    // case 3:joint = &handler.j3;break;
-    // case 4:joint = &handler.j4;break;
-    // case 5:joint = &handler.j5;break;
-    // case 6:joint = &handler.j6;break;
 
-    // default:
-    //     break;
-    // }
-    // if(init)
     handler.initJoints();
 
     puts("J3");
-    handler.searchHome(handler.j3);
+    handler.searchHome(handler.j3,verbose);
     puts("J2");
-    handler.searchHome(handler.j2);
+    handler.searchHome(handler.j2,verbose);
     puts("J1");
-    handler.searchHome(handler.j1);
-    //puts("J4");
-    //handler.searchHome(handler.j4);
+    handler.searchHome(handler.j1,verbose);
+ 
     puts("Waiting for PID to stabilize");
     usleep(15000000);
     EDScorbotJoint *joints[6] = {&handler.j1, &handler.j2, &handler.j3, &handler.j4, &handler.j5, &handler.j6};
