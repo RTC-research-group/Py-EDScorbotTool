d3714fa21c9c4c549c6ceae4f28c3495472eee43
Enrique Piñero <epinerof@us.es>
2022-10-20 17:53:51 +0200

    Added argument parser Cleaned old comments

--- c/src/scripts/reset_all.cpp
+++ c/src/scripts/reset_all.cpp
@@ -1,49 +1,37 @@
 #include "include/EDScorbot.hpp"
 
+#include <argparse/argparse.hpp>
+
 
 int main(int argc, char* argv[])
 {   
-    //int j = atoi(argv[1]);
-    //int ref = atoi(argv[2]);
-    //int init = atoi(argv[3]);
-    char* config_file = argv[1];
+    
+    argparse::ArgumentParser parser("reset");
+    parser.add_argument("-c", "--config_file").help("Optional. Configuration file in JSON format. This file can be used to configure each joint's controller parameters. Default is 'initial_config.json'").default_value(std::string("initial_config.json"));
+    parser.add_argument("-v", "--verbose").help("Increase verbosity of output").default_value(false).implicit_value(true);
+
+    try
+    {
+        parser.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
+
+    const char *config_file = parser.get<std::string>("--config_file").c_str();
+    bool verbose = parser.get<bool>("--verbose");
+
     EDScorbot handler(config_file);
     
     EDScorbotJoint* joint;
-    // switch (j)
-    // {
-    // case 1:joint = &handler.j1;break;
-    // case 2:joint = &handler.j2;break;
-    // case 3:joint = &handler.j3;break;
-    // case 4:joint = &handler.j4;break;
-    // case 5:joint = &handler.j5;break;
-    // case 6:joint = &handler.j6;break;
-    
-    // default:
-    //     break;
-    // }
-   // if(init)
+
     handler.configureInitJoint(handler.j1);
     handler.configureInitJoint(handler.j2);
     handler.configureInitJoint(handler.j3);
     handler.configureInitJoint(handler.j4);
-
-    // puts("J1");
-    // handler.searchHome(handler.j1);
-    // puts("J2");
-    // handler.searchHome(handler.j2);
-    // puts("J3");
-    // handler.searchHome(handler.j3);
-    // puts("J4");
-    // handler.searchHome(handler.j4);
-    
-    // int reads[6];
-    // handler.readJoints(reads);
-
-    // puts("Leido:");
-    // for (int i = 0;i<6; i++){
-    //     printf("J%d: %d\n",i+1,reads[i]);
-    // }
 
     
 
