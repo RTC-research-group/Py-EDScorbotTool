152d6fae868a28a24daa63d0cc2fc431dd035818
Enrique Piñero <epinerof@us.es>
2022-11-02 19:32:06 +0100

    Updated some transformation scripts

--- c/src/python_utils/transformations/angles_to_refs.py
+++ c/src/python_utils/transformations/angles_to_refs.py
@@ -1,67 +1,16 @@
 import numpy as np
 from argparse import ArgumentParser
 import pandas as pd
-import math
+import sys, os
+sys.path.append(os.path.abspath("../../../python"))
+from pyAER import pyEDScorbotTool
 
-def angle_to_ref(motor,angle,strict=False):
-        """
-        Convert angle of motor to reference
-
-        This function takes a motor and an angle and 
-        returns the corresponding reference to said angle
-        for that specific motor.
-
-        Args:
-            motor (int): Number of the motor (1-4)
-            angle (int): angle to be converted
-            strict (boolean): Whether to restrict reference values to ther maximum bounds or not (default is True)
-        
-        Returns:
-            float: Reference that corresponds to the angle given for the given motor or the joint's limit if the calculated reference is above (or below) it
-        """
-        f = lambda x:x
-
-        bounds = [[400,-400],[700,-900],[300,-400],[1583,-1583]]
-        ##############DEPRECATED#############
-        #These are the inverse of the functions that appear in ref_to_angle function
-        
-        # if motor == 1:
-        #     f = lambda x: (-3)*x #https://www.symbolab.com/solver/function-inverse-calculator/inverse%20f%5Cleft(x%5Cright)%3D-%5Cfrac%7B1%7D%7B3%7Dx
-        # elif motor == 2:
-        #     f = lambda x: -(((100*x) - 5300)/11) #https://www.symbolab.com/solver/function-inverse-calculator/inverse%20f%5Cleft(x%5Cright)%3D%20-%5Cfrac%7B11%7D%7B100%7Dx%2B53
-        # elif motor == 3:
-        #     if ((angle >=-66.5) and (angle <= 143.5)):
-        #         f = lambda x: -(((200*x)-2200)/67) #https://www.symbolab.com/solver/function-inverse-calculator/inverse%20f%5Cleft(x%5Cright)%3D-%5Cfrac%7B67%7D%7B200%7Dx%2B11
-        #     else:
-        #         self.alert("Joint 3 out of range. Angle must be between 143.5 and -66.5")
-        #         return
-        # elif motor == 4:
-        #     f = lambda x:-(((50*x)-100)/9) #https://www.symbolab.com/solver/function-inverse-calculator/inverse%20f%5Cleft(x%5Cright)%3D%5Cfrac%7B9%7D%7B50%7Dx%20%2B2
-        #####################################
-
-        if motor == 1:
-            #f = lambda x:-3.1428474*x
-            f = lambda x:-3*x
-        elif motor == 2:
-            #f = lambda x:-8.824695*x
-            f = lambda x:-9.4*x
-        elif motor == 3:
-            #f = lambda x:-3.4044209*x
-            f = lambda x:-3.1*x
-        elif motor ==4:
-            f = lambda x:-17.61158871*x
-        else:
-            return 0
-
-        ret = round(f(angle))
-        # if not(ret < bounds[motor-1][0] and ret > bounds[motor-1][1]) and strict:
-        #     ret = bounds[motor-1][0]*np.sign(ret)
-
-        return ret
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("file",type=str,action="store")
+    parser.add_argument("input_file",type=str,action="store",help="Numpy file (.npy or pickled) with angles in format (q1,q2,q3,q4) to be converted to references")
+    parser.add_argument("--output_file","-o",type=str,action="store",help="Name of the output file",default="refs_out.npy")
+    
     args = parser.parse_args()
     arr = np.load(args.file,allow_pickle=True)
     df = pd.DataFrame(arr)
@@ -69,7 +18,7 @@ if __name__ == "__main__":
     i = 0
     for name, values in df.iteritems():
         #print(name,values)
-        values = angle_to_ref(name+1,values)
+        values = pyEDScorbotTool.angle_to_ref(name+1,values)
         df[i] = values
         i+=1
     
--- c/src/python_utils/transformations/count_to_angle.py
+++ c/src/python_utils/transformations/count_to_angle.py
@@ -1,14 +1,21 @@
 import sys
 import os
 sys.path.append(os.path.abspath("../../../python"))
+sys.path.append(os.path.abspath("../"))
+sys.path.append(os.path.abspath(r"D:\Universidad\Master\Ondrive Cloud\OneDrive - UNIVERSIDAD DE SEVILLA\Trabajo\SMALL\dataset"))
 from argparse import ArgumentParser
 from pyAER import pyEDScorbotTool
 import numpy as np
+from visual_kinematics import RobotSerial
 
-def cont_to_xyz(fname):
-    parser = ArgumentParser()
-    parser.add_argument("file",type=str,action="store")
-    args = parser.parse_args()
+def cont_to_angle(fname):
+    
+    dh_params = np.array([[0.3585, 0.05, -0.5 * np.pi, 23.6*(np.pi/180)],
+                      [-0.098, 0.3, 0., -22*(np.pi/180)],
+                      [0.065, 0.35, 0., 22.4*(np.pi/180)],
+                      [0., 0.22, 0., 0.]])
+    robot = RobotSerial(dh_params)
+
 
     conts = np.array(j.load(open(fname)))
     qs = []
@@ -26,21 +33,40 @@ def cont_to_xyz(fname):
         q2 = -(pyEDScorbotTool.count_to_angle(2,c2) * np.pi/180)
         q3 = pyEDScorbotTool.count_to_angle(3,c3) * np.pi/180
         q4 = pyEDScorbotTool.count_to_angle(4,c4) * np.pi/180
-        theta = np.array([q1,q2,q3,q4])
-        f = robot.forward(theta)      
-        #xyz.append(DirecKinScorbot(q1,q2,q3,q4))
-        xyz.append(f.t_3_1.reshape([3, ]))
+        
         qs.append([q1,q2,q3,q4])
         cs.append([c1,c2,c3,c4])
         timestamps.append(ts)
-    return np.array(xyz),np.array(qs),np.array(cs),np.array(timestamps)
-dh_params = np.array([[0.3585, 0.05, -0.5 * np.pi, 23.6*(np.pi/180)],
-                      [-0.098, 0.3, 0., -22*(np.pi/180)],
-                      [0.065, 0.35, 0., 22.4*(np.pi/180)],
-                      [0., 0.22, 0., 0.]])
-robot = RobotSerial(dh_params)
+    return np.array(qs),np.array(cs),np.array(timestamps)
 
 
 
-xyz,qs,cs,timestamps= cont_to_xyz('ejez_0.3-0.6_10sleep_nohome_out.json')
-timestamps = timestamps - timestamps[0]
+if __name__== '__main__':
+    
+    parser = ArgumentParser()
+    parser.add_argument("input_file",type=str,action="store",help="JSON file with counters output in EDScorbot format n tuples of (j1,j2,j3,j4,j5,j6,timestamp) elements")
+    parser.add_argument("--output_file","-o",type=str,action="store",help="Name of the output file",default="angles_out.npy")
+    parser.add_argument("--include_timestamps","-ts",type=bool,action="store_true",help="Include timestamps in the output file. Output will be in (q1,q2,q3,q4,timestamp) format",default=False)
+    args = parser.parse_args()
+
+    cont_file = args.input_file
+    output_file = args.output_file
+    include_timestamps = args.include_timestamps
+    qs,cs,timestamps= cont_to_angle(cont_file)
+    timestamps = timestamps - timestamps[0]
+
+    if include_timestamps:
+        import pandas
+        df = pandas.DataFrame(qs)
+        df[4] = cs
+        np.save(output_file,df.to_numpy())
+        
+    else:
+        np.save(output_file,qs)
+
+    print("Saved output to file{}".format(output_file))
+    print("Timestamp included: {}".format("Yes" if include_timestamps else "No"))
+
+
+
+
