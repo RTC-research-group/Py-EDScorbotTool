c198f190efd67380b38a044ee6ea6c282e7ccade
Enrique Piñero <epinerof@us.es>
2022-11-02 19:32:00 +0100

    Added new files to be worked on in visualization and transformation directories

---
+++ c/src/python_utils/transformations/refs_to_angles.py
@@ -0,0 +1,30 @@
+import numpy as np
+from argparse import ArgumentParser
+import pandas as pd
+import sys, os
+sys.path.append(os.path.abspath("../../../python"))
+from pyAER import pyEDScorbotTool
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("input_file",type=str,action="store",help="Numpy file (.npy or pickled) with references in format (rj1,rj2,rj3,rj4) to be converted to angles")
+    parser.add_argument("--output_file","-o",type=str,action="store",help="Name of the output file",default="angles_out.npy")
+    
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    arr = np.load(input_file,allow_pickle=True)
+    df = pd.DataFrame(arr)
+    l = []
+    i = 0
+    for name, values in df.iteritems():
+        #print(name,values)
+        values = pyEDScorbotTool.ref_to_angle(name+1,values)
+        df[i] = values
+        i+=1
+    
+    
+    np.save(output_file,df.to_numpy())
