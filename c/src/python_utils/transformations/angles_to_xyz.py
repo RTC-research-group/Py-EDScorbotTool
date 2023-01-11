24447b2ac37fed0832b1a21d7c681a2dc513958a
Enrique Piñero <epinerof@us.es>
2023-01-11 12:02:35 +0100

    Added path fix

--- c/src/python_utils/transformations/angles_to_xyz.py
+++ c/src/python_utils/transformations/angles_to_xyz.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 import pandas as pd
 import sys, os
 sys.path.append(os.path.abspath("../../../../python"))
+sys.path.append(os.path.abspath("/media/HDD/home/enrique/Proyectos/SMALL/dataset/"))
 from pyAER import pyEDScorbotTool
 from visual_kinematics.RobotSerial import RobotSerial
 
