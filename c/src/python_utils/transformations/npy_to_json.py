139c04aa176eb41dcefd22d0afa793d1f1222bbf
Enrique Piñero <epinerof@us.es>
2023-01-13 13:56:56 +0100

    For future use

--- c/src/python_utils/transformations/npy_to_json.py
+++ c/src/python_utils/transformations/npy_to_json.py
@@ -8,5 +8,24 @@ if __name__== '__main__':
     args = parser.parse_args()
     arr = np.load(args.file,allow_pickle=True)
     f = open("./converted.json","w")
-    js = json.dump(arr.tolist(),f)
+    #js = json.dump(arr.tolist(),f)
+    try:
+        d_js = {
+            "J1":arr[:,0].tolist(),
+            "J2":arr[:,1].tolist(),
+            "J3":arr[:,2].tolist(),
+            "J4":arr[:,3].tolist(),
+            "J5":arr[:,4].tolist(),
+            "J6":arr[:,5].tolist()
+        }
+    except Exception as e:
+        d_js = {
+            "J1":arr[:,0].tolist(),
+            "J2":arr[:,1].tolist(),
+            "J3":arr[:,2].tolist(),
+            "J4":arr[:,3].tolist(),
+        }
+
+    js = json.dump(d_js,f,indent=4)
+    f.close()
     
