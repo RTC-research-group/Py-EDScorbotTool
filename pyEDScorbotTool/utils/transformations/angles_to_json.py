eb73d440141513cc8321ccaf012dbbc643e9811c
Enrique Piñero <epinerof@us.es>
2023-01-27 22:51:11 +0100

    Now df array is copied before anything and operations are performed on the copy

--- pyEDScorbotTool/utils/transformations/angles_to_json.py
+++ pyEDScorbotTool/utils/transformations/angles_to_json.py
@@ -9,12 +9,14 @@ from .angles_to_refs import angles_to_refs
 from .pad_trajectory import pad
 import json
 
-def angles_to_json(df):
-    df[0] = -df[0]
-    df[1] = -df[1]
+def angles_to_json(df,visual=False):
+    df2 = deepcopy(df)
+    if visual:
+        df2[:,0] = -df2[:,0]
+        df2[:,1] = -df2[:,1]
     
-    df = angles_to_refs(df) #angles to refs
-    new_arr = pad(df.to_numpy()) #pad reference trajectory
+    df2 = angles_to_refs(df2) #angles to refs
+    new_arr = pad(df2.to_numpy()) #pad reference trajectory
     return new_arr
 
 if __name__ == "__main__":
@@ -22,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("input_file",type=str,action="store",help="Numpy file (.npy or pickled) with angles in format (q1,q2,q3,q4) to be converted to references")
     parser.add_argument("--output_file","-o",type=str,action="store",help="Name of the output file",default="6dims_converted_out.npy")
     
+
     args = parser.parse_args()
     input_file = args.input_file
     output_file = args.output_file
