9782c18040284a86352e24211d1244db515d5595
Enrique Piñero <epinerof@us.es>
2024-08-30 08:26:44 +0200

    Changed read_joints to publish read data in EDScorbot/trajectory Before it printed to console

--- c/src/scripts/read_joints.cpp
+++ c/src/scripts/read_joints.cpp
@@ -1,51 +1,49 @@
-#include "include/EDScorbot.hpp"
+#include "trajectory/utils.h"
+
 
 
 int main(int argc, char *argv[])
 {
-    int j = atoi(argv[1]);
-    int ref = atoi(argv[2]);
-    int init = atoi(argv[3]);
-    char *config_file = argv[4];
-    EDScorbot handler(config_file);
-
-    EDScorbotJoint *joint;
-    switch (j)
+    argparse::ArgumentParser parser("read_joints");
+    parser.add_argument("-c", "--config_file").help("Optional. Configuration file in JSON format. This file can be used to configure each joint's controller parameters. Default is 'initial_config.json'").default_value(std::string("/home/root/initial_config.json"));
+    parser.add_argument("-v", "--verbose").help("Increase verbosity of output").default_value(false).implicit_value(true);
+    
+    try
     {
-    case 1:
-        joint = &handler.j1;
-        break;
-    case 2:
-        joint = &handler.j2;
-        break;
-    case 3:
-        joint = &handler.j3;
-        break;
-    case 4:
-        joint = &handler.j4;
-        break;
-    case 5:
-        joint = &handler.j5;
-        break;
-    case 6:
-        joint = &handler.j6;
-        break;
-
-    default:
-        break;
+        parser.parse_args(argc, argv);
     }
-    if (init)
-        handler.initJoints();
+    catch (const std::runtime_error &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
 
-    handler.sendRef(ref, *joint);
+    const char *config_file = parser.get<std::string>("--config_file").c_str();
+    mosquitto_lib_init();
+    usleep(10000000); // Wait for 10 seconds to let the arm come back to home position
+    struct mosquitto *mosq;
+    mosq = mosquitto_new(NULL, true, 0);
+    char ip[20] = "192.168.1.104";
+    init_mqtt_client(mosq, ip);
+    char mqtt_msg[MAX_MQTT_MSG];
+    EDScorbot handler(config_file);
+   
+    
     char c = 'a';
     int reads[6];
 
     while (c != EOF)
     {
         handler.readJoints_counter(reads);
-        printf("J%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\t\r", 1, reads[0],2, reads[1],3, reads[2],4, reads[3],5, reads[4],6, reads[5]);
+        // printf("J%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\tJ%d: %d\t\r", 1, reads[0],2, reads[1],3, reads[2],4, reads[3],5, reads[4],6, reads[5]);
+        snprintf(mqtt_msg,MAX_MQTT_MSG,"[%d,%d,%d,%d,%d,%d]", reads[0], reads[1], reads[2], reads[3], reads[4], reads[5]);
+        int ret = publish(mosq, mqtt_msg, strlen(mqtt_msg), std::string("EDScorbot/trajectory").c_str());
+
         fflush(stdout);
+        usleep(100000);
+        
+        
     
     }
 
